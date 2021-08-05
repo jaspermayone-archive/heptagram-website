ec194d734f0230ab603aa8105afab2c5aaa6417e
j-dogcoder <j.dogcoder@gmail.com>
2021-08-05 15:43:28 -0400

    Revert "Added the SetupTabs script"
    This reverts commit 3b9ab42de032fa6c99d72d0330390821a4a449ce.

--- main.js
+++ main.js
@@ -22,26 +22,3 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
-
-function setupTabs() {
-    document.querySelectorAll('.tabs__button').forEach(button => {
-        button.addEventListener('click', () => {
-            const sideBar = button.parentElement
-            const tabsContainer = sideBar.parentElement
-            const tabsNumber = button.dataset.forTab
-            const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabsNumber}"]`)
-
-            sideBar.querySelectorAll(".tabs__button").forEach(button => {
-                button.classList.remove("tabs__button--active")
-            })
-            tabsContainer.querySelectorAll(".tabs__content").forEach(tab => {
-                tab.classList.remove("tabs__content--active")
-            })
-            button.classList.add("tabs__button--active")
-            tabToActivate.classList.add("tabs__content--active")
-        })
-    })
-}
-document.addEventListener('DOMContentLoaded', () => {
-    setupTabs()
-})
