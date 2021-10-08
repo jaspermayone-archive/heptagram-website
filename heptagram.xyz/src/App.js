8dae02cfd4370791010d04e85fe05125b792ead1
MidouWebDev <contact.midoudev@gmail.com>
2021-10-08 20:05:03 +0100

    Created the Home Page

--- heptagram.xyz/src/App.js
+++ heptagram.xyz/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Header from './components/Header';
+import BotMenu from './assets/bot-menu.PNG';
+import Footer from './components/Footer';
+// import { Redirect, Route, Router } from 'react-router-dom';
+// import HomePage from './pages/HomePage';
+// import BotCommandsPage from './pages/BotCommandsPage';
+// import ContributorsPage from './pages/ContributorsPage';
+// import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
+// import TermsOfServicesPage from './pages/TermsOfServicesPage';
+// import Error404Page from './pages/Error404Page';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <>
+      <div className="App">
+        <Header />
+      </div>
+      <div class="description">
+        <div class="left">
+            <h1>Heptagram is the all-in-one Discord Bot for your server!</h1>
+            <p>
+                Heptagram is the All-In-One, Open-Source Discord Bot with the goal to be the single bot needed for any
+                server.
+            </p>
+            <a
+                href="https://discord.com/oauth2/authorize?client_id=783073095036043274&permissions=122682682614&scope=bot%20messages.read%20applications.commands">
+                <button class="invite-the-bot">
+                    Invite the bot &nbsp;
+                    <i class="fab fa-discord"></i>
+                </button>
+            </a>
+        </div>
+        <div class="right">
+            <img src={BotMenu} alt="Bot Menu" class="bot" />
+        </div>
+    </div>
+    <div class="features">
+        <h2>Features</h2>
+        <div class="feature">
+            <i class="fas fa-shield-alt fa-2x"></i>
+            <a class="feature-link" href="/bot-commands">
+                <p>Moderation</p>
+            </a>
+        </div>
+        <div class="feature">
+            <i class="fas fa-robot fa-2x"></i>
+            <a class="feature-link" href="/bot-commands">
+                <p>Automation</p>
+            </a>
+        </div>
+        <div class="feature">
+            <i class="fas fa-music fa-2x"></i>
+            <a class="feature-link" href="/bot-commands">
+                <p>Music</p>
+            </a>
+        </div>
+        <div class="feature">
+            <i class="far fa-laugh-beam fa-2x"></i>
+            <a class="feature-link" href="/bot-commands">
+                <p>Fun</p>
+            </a>
+        </div>
+    </div>
+    <div class="before-footer">
+        <p class="invite">
+            Enhance any Discord server now, for free!
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+            href="https://discord.com/oauth2/authorize?client_id=783073095036043274&permissions=122682682614&scope=bot%20messages.read%20applications.commands">
+            <button class="invite-the-bot reminder">
+                    Invite the bot &nbsp;
+                    <i class="fab fa-discord"></i>
+            </button>
         </a>
-      </header>
     </div>
-  );
+    <Footer />
+      </>
+  )
 }
 
-export default App;
+export default App
