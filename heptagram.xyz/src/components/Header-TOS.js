d091e1ca602e6e5e2877d6bd1399a3cef944be57
MidouWebDev <contact.midoudev@gmail.com>
2021-10-09 21:11:09 +0100

    Created Terms Of Service Page

---
+++ heptagram.xyz/src/components/Header-TOS.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderTOS() {
+    return (
+        <div>
+            <h1>Website Terms and Conditions of Use</h1>
+        </div>
+    )
+}
+
+export default HeaderTOS
