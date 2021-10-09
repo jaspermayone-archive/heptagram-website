fdfebe15e5f6834cb4bafddffcdd6d2c2cab2583
MidouWebDev <contact.midoudev@gmail.com>
2021-10-09 22:07:06 +0100

    Created Contributors Page

---
+++ heptagram.xyz/src/components/HeaderDevs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import HeptagramLogo from '../assets/full-heptagram-logo.png'
+
+function HeaderDevs() {
+    return (
+        <div class="header">
+            <img src={HeptagramLogo} alt="heptagram-logo" class="logo" />
+            <h1>Developers</h1>
+        </div>
+    )
+}
+
+export default HeaderDevs
+
