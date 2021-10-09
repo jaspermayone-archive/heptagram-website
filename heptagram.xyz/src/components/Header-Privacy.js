c6af2f3c562658da196c1bcbe1fa8b03e9489d50
MidouWebDev <contact.midoudev@gmail.com>
2021-10-09 21:21:46 +0100

    Created Privacy Policy Page

---
+++ heptagram.xyz/src/components/Header-Privacy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderPrivacy() {
+    return (
+        <div>
+            <h1>Privacy Policy for Heptagram-Bot</h1>
+        </div>
+    )
+}
+
+export default HeaderPrivacy
