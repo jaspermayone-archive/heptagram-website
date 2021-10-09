f6b485c491dc26e7fc9916f3df1a21e583b98bb7
MidouWebDev <contact.midoudev@gmail.com>
2021-10-09 21:11:09 +0100

    Created Terms Of Service Page

---
+++ heptagram.xyz/src/components/Footer-Legals.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function FooterLegals() {
+    return (
+        <div>
+            <hr/>
+            <p class="credit-midouwebdev">This website was designed and coded by the Heptagram Website team, lead by <a class="main-dev" href="https://github.com/MidouWebDev">MidouWebDev</a></p>
+            <br />
+            <br />
+            <hr />
+            <br/>
+            <br />
+        </div>
+    )
+}
+
+export default FooterLegals
