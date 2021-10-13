267c68e709e0cee6f45485bb8cdba982544eef0d
Anubhav Gupta <mail.anubhav06@gmail.com>
2021-10-14 01:22:13 +0530

    Legals Pages fixed

--- heptagram.xyz/src/components/Footer-Legals.js
+++ heptagram.xyz/src/components/Footer-Legals.js
@@ -4,11 +4,8 @@ function FooterLegals() {
     return (
         <div>
             <hr/>
-            <p class="credit-midouwebdev">This website was designed and coded by the Heptagram Website team, lead by <a class="main-dev" href="https://github.com/MidouWebDev">MidouWebDev</a></p>
-            <br />
-            <br />
+            <p className="credit-midouwebdev">This website was designed and coded by the Heptagram Website team, lead by <a className="main-dev" href="https://github.com/MidouWebDev">MidouWebDev</a></p>
             <hr />
-            <br/>
             <br />
         </div>
     )
