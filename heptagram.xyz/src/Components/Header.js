be1e87df0a3491184e7c70b276100426473d1080
Anubhav Gupta <mail.anubhav06@gmail.com>
2021-10-14 00:15:57 +0530

    Home Page fixed

--- heptagram.xyz/src/Components/Header.js
+++ heptagram.xyz/src/Components/Header.js
@@ -4,28 +4,30 @@ import HeptagramLogo from '../assets/full-heptagram-logo.png';
 
 function Header() {
     return (
-        <nav class="navbar">
-            <img src={HeptagramLogo} alt="Heptagram Logo" class="logo" />
-            <ul class="nav-menu">
-                <li class="nav-item second">
-                    <a href="https://heptagram.xyz" class="nav-link home active">Home</a>
+        <header className="header">
+        <nav className="navbar">
+            <img src={HeptagramLogo} alt="Heptagram Logo" className="logo" />
+            <ul className="nav-menu">
+                <li className="nav-item second">
+                    <a href="https://heptagram.xyz" className="nav-link home active">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a href="https://discord.gg/HSupF99kpq" class="nav-link server">Support Server</a>
+                <li className="nav-item">
+                    <a href="https://discord.gg/HSupF99kpq" className="nav-link server">Support Server</a>
                 </li>
-                <li class="nav-item">
-                    <a href="/bot-commands" class="nav-link commands">Commands</a>
+                <li className="nav-item">
+                    <a href="/bot-commands" className="nav-link commands">Commands</a>
                 </li>
-                <li class="nav-item">
-                    <a href="https://api.heptagram.xyz" class="nav-link api">API</a>
+                <li className="nav-item">
+                    <a href="https://api.heptagram.xyz" className="nav-link api">API</a>
                 </li>
             </ul>
-            <div class="hamburger">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
+            <div className="hamburger">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
             </div>
         </nav>
+        </header>
     )
 }
 
