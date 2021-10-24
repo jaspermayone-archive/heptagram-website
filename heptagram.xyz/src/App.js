17e42bba7fa7808224e58ba2c95e890e6ef33e9d
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2021-10-24 13:30:36 -0400

    Set default router to /home

--- heptagram.xyz/src/App.js
+++ heptagram.xyz/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, BrowserRouter as Router} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 
 import HomePage from './Pages/HomePage';
 import BotCommandsPage from './Pages/BotCommandsPage';
@@ -8,15 +8,18 @@ import PrivacyPolicyPage from './Pages/PrivacyPolicyPage';
 import TermsOfServicesPage from './Pages/TermsOfServicesPage';
 
 function App() {
-    return (
-      <Router>
-          <Route path="/home" component={HomePage} exact />
-          <Route path="/contributors" component={ContributorsPage} />
-          <Route path="/bot-commands" component={BotCommandsPage} />
-          <Route path="/privacy-policy" component={PrivacyPolicyPage} />
-          <Route path="/terms-of-service" component={TermsOfServicesPage} />
-      </Router>
-    )
+	return (
+		<Router>
+			<Route exact path='/'>
+				<Redirect to='/home' />
+			</Route>
+			<Route path='/home' component={HomePage} exact />
+			<Route path='/contributors' component={ContributorsPage} />
+			<Route path='/bot-commands' component={BotCommandsPage} />
+			<Route path='/privacy-policy' component={PrivacyPolicyPage} />
+			<Route path='/terms-of-service' component={TermsOfServicesPage} />
+		</Router>
+	);
 }
 
-export default App
+export default App;
